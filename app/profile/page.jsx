162ac29f37780c530e3ac6eddaf4bfd6af14dcf0
oussamalay99 <oussamalay99@gmail.com>
2025-07-13 22:13:55 +0100

    finish profile and edit and delete forms

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,10 +6,8 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
   const { data: session } = useSession();
-
+  const router = useRouter();
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -21,6 +19,27 @@ const MyProfile = () => {
     };
     if (session?.user.id) fetchPosts();
   }, [session?.user.id]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method:'DELETE'
+        })
+        const filteredPosts = posts.filter((p) => p._id !== post._id)
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  };
   return (
     <Profile
       name="My"
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,12 +6,16 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const {data: session} = useSession();
+  const pathname = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => setCopied(""), 3000);
-  }
+  };
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -48,9 +52,28 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       </div>
 
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p className="font-inter text-sm blue_gradient cursor-pointer"
-      onClick={() => handleTagClick && handleTagClick(post.ag)}
-      >{post.tag}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.ag)}
+      >
+        #{post.tag}
+      </p>
+      {session?.user.id === post.creator._id && pathname === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
